3902603ecedcb2adb45a5c5a78fb0626a35d48dc
mokolodi1 <mokolodi1@gmail.com>
2015-03-16 01:24:46 +0100

    worked on send_ray.c

--- srcs/send_ray.c
+++ srcs/send_ray.c
@@ -13,17 +13,34 @@
 #include "wolf3d.h"
 
 /*
-** vertical = finds walls that are vertical on the map
+** set_vertical = finds walls that are vertical on the map
 */
 
-static void			set_vertical(t_ray *ray, t_map *map, t_point start
-								 , double slope)
+static void			set_vertical(t_ray *ray, t_map *map, t_point *start
+								 , double viewing_angle)
 {
-	
-	// check to see if slope is bad
-	(void)ray;
-	(void)map;
-	(void)start;
+	t_point			current;
+	int				delta_y;
+	int				delta_x;
+
+	current.x = math.floor(start->current.x) + (viewing_angle < M_PI);
+	delta_x = (viewing_angle < M_PI ? 1 : -1);
+	delta_y = sin(viewing_angle) / cos(viewing_angle);
+	y += delta_y * (viewing_angle < M_PI
+					? (start->x - floor(start->x) : (ceil(start->x) - start->x)));
+	while ((viewing_angle < M_PI ? current.x >= 4 : current.x <= map->width)
+		   && y >= 0 && y <= map->width)
+	{
+		if (map->data[(int)y][current.x]
+			&& map[/* stuff */][current.x])
+		{
+			ray->distance = distance_between(start, current);
+			ray->direction = (current.y - floor(current.y) > 0 ? NORTH : SOUTH);
+			return ;
+		}
+		y += delta_y;
+		current.x += delta_x;
+	}
 }
 
 void				send_ray(t_ray *ray, t_map *map, t_point *location
@@ -34,8 +51,10 @@ void				send_ray(t_ray *ray, t_map *map, t_point *location
 
 	vertical.distance = -1;
 	horizontal.distance = -1;
-	set_vertical(&vertical, map, location, slope);
-	set_horizontal(&horizontal, map, location, 1/slope);
+	if (cos(viewing_angle))
+		set_vertical(&vertical, map, location, viewing_angle);
+	if (sin(viewing_angle))
+		set_horizontal(&horizontal, map, location, viewing_angle);
 	if (vertical.distance != -1 && horizontal.distance != -1)
 		*ray = vertical.distance < horizontal.distance
 			? vertical, horizontal;
