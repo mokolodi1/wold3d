7566622256ec3c9879e751e14af0466667d134b2
mokolodi1 <mokolodi1@gmail.com>
2015-03-21 14:55:38 +0100

    changed error messages, added link to spreadsheet

--- srcs/main.c
+++ srcs/main.c
@@ -39,7 +39,14 @@ int					main(int argc, char **argv)
 	t_map			map;
 
 	if (argc != 2)
-		ft_putendl_exit("Incorrect arguments: ./wolf3d map.wolf", 1);
+	{
+		ft_putendl("Incorrect arguments: ./wolf3d map.wolf");
+		ft_putendl("To help make compatible maps:");
+		ft_putstr("https://docs.google.com/spreadsheets/d/1aRdje8c_uhFyBkuTSu");
+		ft_putstr("m6NVpipAV-pKIJK65KAC3L9hg/edit?usp=sharing");
+		ft_putchar('\n');
+		exit(1);
+	}
 	ft_bzero(&map, sizeof(t_map));
 	read_map(&map, argv[1]);
 	print_map(&map);
